a268c672538e9f69c572f82c37f3d2f6413355dd
Yoshizak1 <yoshizaki02@hotmail.com>
2024-02-11 22:58:51 -0300

    add-new-card

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -3,19 +3,24 @@ import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 import { useState } from "react";
 
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
+
 export function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: Math.random(),
       date: new Date(),
-      content: "Hello World",
-    },
-    {
-      id: 2,
-      date: new Date(),
-      content: "nota 2",
-    },
-  ]);
+      content,
+    };
+
+    setNotes([newNote, ...notes]);
+  }
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -32,7 +37,7 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {notes.map((note) => {
           return <NoteCard key={note.id} note={note} />;
